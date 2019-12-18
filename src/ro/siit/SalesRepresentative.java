6f6ad11aaf0988e4be5c7f7ccc17de9ef0d15e0c
loredanaisuf <44907592+loredanaisuf@users.noreply.github.com>
2019-12-19 00:34:11 +0200

    Second commit
    with Javadoc

--- src/ro/siit/SalesRepresentative.java
+++ src/ro/siit/SalesRepresentative.java
@@ -5,12 +5,23 @@ public class SalesRepresentative {
     private int quota;
     private int totalValue;
 
+    /**
+     * this is the constructor
+     * @param n : its value is assigned to numberOfSales field
+     * @param q : its value it assigned to quota field
+     * the product between n and q is assigned to totalValue field
+     */
     public SalesRepresentative(int n, int q){
         numberOfSales=n;
         quota=q;
         totalValue=n*q;
     }
 
+    /**
+     *this is the method where I'm doing the sorting of an array of SalesRepresentative objects in descending order.
+     * @param reps represents an array of SalesRepresentative
+     * @return the array sorted
+     */
     public SalesRepresentative[] sort(SalesRepresentative[] reps){
         int i,j;
         SalesRepresentative temp;
@@ -25,6 +36,7 @@ public class SalesRepresentative {
         return reps;
     }
 
+    @Override
     public String toString(){
         return "NumberOfSales:"+numberOfSales+", Quota: "+quota+", Total: "+totalValue;
     }
